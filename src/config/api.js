dbf8e5a4d965e85270aed4add255a214ade1e991
LucasPenido <lpenidoa@me.com>
2019-11-19 15:39:55 -0300

    Updating package.json

--- src/config/api.js
+++ src/config/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const baseUrl = 'http://fastpaypi2.ml'
 const authUrl = baseUrl + ':3001'
 const purchaseUrl = baseUrl + ':5000'
+const productsUrl = baseUrl + ':3000'
 
 const auth = axios.create({
   baseURL: authUrl
@@ -12,7 +13,12 @@ const purchase = axios.create({
   baseURL: purchaseUrl
 });
 
+const products = axios.create({
+  baseURL: productsUrl
+})
+
 export default {
   auth,
-  purchase
+  purchase,
+  products,
 }
